77a29b6fbdd867af38ff4d46017cf6b6d81126eb
Dennis Kurtz <dennis-kurtz@uiowa.edu>
2020-11-16 18:33:52 -0600

    Testing initial commit

---
+++ build/CMakeFiles/rs-hello-realsense.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rs-hello-realsense.dir/rs-hello-realsense.cpp.o"
+  "rs-hello-realsense.pdb"
+  "rs-hello-realsense"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rs-hello-realsense.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
